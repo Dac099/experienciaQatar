dff43223b1d3704f07727b295fd06e5d575ef849
David Ceja Zapata <dac099@pop-os.localdomain>
2022-09-28 15:48:40 -0500

    landing page

---
+++ src/public/js/window-modal.js
@@ -0,0 +1,12 @@
+const modal = document.getElementById("account-modal");
+const btn = document.getElementById("signup");
+const spanClose = document.getElementsByClassName("close");
+
+btn.onclick = () => modal.style.display = "block";
+spanClose.onclick = () => modal.style.display = "none";
+
+window.onclick = (e) => {
+  if(e.target == modal){
+    modal.style.display = "none";
+  }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,44 +1,23 @@
 const { Router } = require('express');
 const { db, auth } = require('../firebase.js');
 const { signin, signup, getUser, getAllUsers, deleteUser} = require('./users.router.js');
+const { getUrlImages } = require('../storage.js');
 
 const router = Router();
 
 router.get('/', async (req, res) => {
+  const urlImgs = await getUrlImages();
+  
   res.render('landing', {
     title: 'Experiencia Qatar',
     message: 'Landing page',
-    logged: false
+    logged: false,
+    banner: urlImgs.banner,
+    icon: urlImgs.logo
   });
 });
 
 router.get('/signin', signin);
 router.post('/signup', signup);
-router.get('/get-user/:userID', getUser);
-router.get('/delete-user/:userID', deleteUser);
-
-router.get('/get-user/:userID', async (req, res) => {
-  // const user = await db.collection('usuarios').doc(req.params.userID).get();
-  // console.log(user.data());
-  const user = await auth.getUser(req.params.userID);
-  console.log(user);
-
-  res.send('Usuario obtenido');
-})
-
-router.get('/delete-user/:userID', async (req, res) => {
-  await db.collection('usuarios').doc(req.params.userID).delete();
-  console.log('Usuario eliminado');
-
-  auth.deleteUser()
-
-  res.send('Usuario eliminado');
-});
-
-router.post('/update-user/:userID', async (req, res) => {
-  await db.collection('usuarios').doc(req.params.userID).update(req.body);
-
-  res.send('Usuario actualizado');
-});
 
 module.exports = router;
